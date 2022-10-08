439653ad3384066b5a5682b1e7d28d6d37e31b13
tdhood <taylordannhood@gmail.com>
2022-10-07 18:07:02 -0700

    GOT MY 6TH STAR!!

--- 3/Day3Puzzle2.py
+++ 3/Day3Puzzle2.py
@@ -1,57 +1,82 @@
+
 '''
+find the life support rating, with is Oxygen Generator Rating x CO2 Scrubber rating
 Oxygen Generator Rating = Most bit values
 CO2 Scrubber rating = Least bit values
 '''
+'''
+1.count all the 0 and 1 in the first column.
+2.find the most
+3.remove the least
+4.count all the 0 and 1 in second column.
+5.repeat 2-3
 
-with open(r'C:\Users\taylo\Documents\Coding\Advent2021\3\day3data.py') as f:
+'''        
+with open('/home/kestrel/Documents/CodingProjects/AdventOfCode2021/3/day3data.py') as f:
     lines = f.readlines()
+    
 
+def filter_most(line_list, index):
+    if len(line_list) == 1:
+        print('02', line_list)
+        return line_list
 
-counts_by_column = [
-    [0, 0], # In the first column, 0 "0"s, and 0 "1"s
-    [0, 0], # In the second column, 0 "0"s, and 0 1"s...
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0], # In the 12th column, 0 "0"s, and 0 "1"s...
-]
+    counter_of_0 = 0
+    counter_of_1 = 0
+    
+    for line in line_list:
+        if line[index] == "0":
+            counter_of_0 += 1
+        if line[index] == "1":
+            counter_of_1 += 1
+    
 
-ox = []
-co = []
-lines_ox = lines
-lines_co = lines
+    if counter_of_1 >= counter_of_0:
+        filtered = [line for line in line_list if line[index]=="1"]
         
-for line in lines:
-    for column, bit in enumerate(line):
-        if bit == '0':
-            counts_by_column[column][0] += 1
-        if bit == '1':
-            counts_by_column[column][1] += 1
+    else:
+        filtered = [line for line in line_list if line[index]=="0"]
+     
 
-print(counts_by_column)            
-##for i, count in enumerate(counts_by_column):
-##    if count[0] > count[1]:
-##        while (len(lines_ox)) > 1:
-##            for line in lines_ox:
-##                if line[i] == '1':
-##                    lines_ox.remove(line)
-##    if count[0] < count[1]:
-##        while (len(lines_ox)) > 1:
-##            for line in lines_ox:
-##                if line[i] == '0':
-##                    lines_ox.remove(line)
-                    
-##        for line in lines:
-##            if line[0] == '0':
-##                ox.append(line)
-##            else:
-##                co.append(line)
-                
- 
+    return filter_most(filtered, index + 1)
 
+def filter_least(line_list, index):
+    if len(line_list) == 1:
+       
+        return line_list
+
+    counter_of_0 = 0
+    counter_of_1 = 0
+    
+    for line in line_list:
+        if line[index] == "0":
+            counter_of_0 += 1
+        if line[index] == "1":
+            counter_of_1 += 1
+    
+
+    if counter_of_0 <= counter_of_1:
+        filtered = [line for line in line_list if line[index]=="0"]
+        
+    else:
+        filtered = [line for line in line_list if line[index]=="1"]
+        
+    return filter_least(filtered, index + 1)
+    
+
+o2 = filter_most(lines, 0)
+num_o2 = int(o2[0][:-1],2)
+
+
+co2 = filter_least(lines, 0)
+num_co2 = int(co2[0][:-1],2)
+
+
+life_support_rating = num_o2 * num_co2
+
+
+
+     
+
+
+    
